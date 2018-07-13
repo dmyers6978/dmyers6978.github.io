f251051c1865059f7eb9c4435c26d188e04e4e8d
Dustin Myers <mye15011@byui.edu>
2018-07-13 06:01:37 -0600

    jquery fix

--- CIT230/lesson-12/scripts/accordian.js
+++ CIT230/lesson-12/scripts/accordian.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
+    $(".description").hide();
     $("#bike1").click(function(){
+        $(".description").hide("slow");
         $("#desc1").slideToggle("slow");
+        });
     });
-});
+
+    $(document).ready(function(){
+    $("#bike2").click(function(){
+        $(".description").hide("slow");
+        $("#desc2").slideToggle("slow");
+        });
+    });
+
+    $(document).ready(function(){
+    $("#bike3").click(function(){
+        $(".description").hide("slow");
+        $("#desc3").slideToggle("slow");
+        });
+    });
+
+
